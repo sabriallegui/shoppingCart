5a5051ba899e35d5c3be79e7c90e6e580fa82d6a
sabriallegui <soloplayasabry@gmail.com>
2022-09-29 22:27:40 +0100

    adding dynamic pricing

--- scripts.js
+++ scripts.js
@@ -6,7 +6,11 @@ console.log('plus-btn', plusOne);
 for (let plus of plusOne) {
     plus.addEventListener('click', () => {
         plus.nextElementSibling.innerText++;
+
+        price()
+
     })
+
 
 }
 //minus button
@@ -19,6 +23,8 @@ for (let minus of minusOne) {
     minus.addEventListener('click', () => {
         if (minus.previousElementSibling.innerText > 0)
             minus.previousElementSibling.innerText--
+        price()
+
     })
 
 }
@@ -32,6 +38,8 @@ console.log('delete-btn', deletebtn);
 for (let i = 0; i < deletebtn.length; i++) {
     deletebtn[i].addEventListener('click', () => {
         deletebtn[i].parentElement.parentElement.parentElement.remove()
+        price()
+
 
     })
 }
@@ -53,3 +61,22 @@ var imagesss = document.querySelector('#imgClickAndChange3');
 imagesss.onclick = function () {
     imagesss.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Love_Heart_SVG.svg/2258px-Love_Heart_SVG.svg.png';
 };
+
+
+// total price 
+
+function price() {
+    var Quantity = document.getElementsByClassName('QTY');
+    var elementPrice = document.getElementsByClassName('total-price');
+
+    console.log('Quantity', Quantity)
+    console.log('elementPrice', elementPrice)
+    let some = 0;
+
+    for (let i = 0; i < elementPrice.length; i++) {
+        some += Quantity[i].innerText + elementPrice[i].innerText
+    }
+    document.getElementById('prix-totalx').innerText = some
+}
+
+
